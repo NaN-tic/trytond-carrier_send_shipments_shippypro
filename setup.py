fb06abed4b624e86577582cc17599f487d9a2e04
Raimon Esteve <raimonesteve@gmail.com>
2025-06-19 14:01:10 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -9,7 +9,13 @@ from configparser import ConfigParser
 
 MODULE = 'carrier_send_shipments_shippypro'
 PREFIX = 'nantic'
-MODULE2PREFIX = {}
+MODULE2PREFIX = {
+    'carrier_api': 'nantic',
+    'carrier_send_shipments': 'nantic',
+    'stock_delivery': 'nantic',
+    'stock_comment': 'nantic',
+    'stock_origin': 'nantic',
+}
 
 
 def read(fname):
@@ -64,12 +70,12 @@ if minor_version % 2:
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='',
+    description='%s' % MODULE,
     long_description=read('README'),
     author='NaN·tic',
     author_email='info@nan-tic.com',
     url='http://www.nan-tic.com/',
-    download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
+    download_url="https://github.com/Nan-tic/trytond-%s" % MODULE,
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
         'trytond.modules.%s' % MODULE,
@@ -77,7 +83,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
